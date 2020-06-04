5f798ae9b5a4c5279b9d231cfe8598213bc79e12
thameem7907 <66255704+thameem7907@users.noreply.github.com>
2020-06-04 05:29:35 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var fixedrect,movingrect
+function setup() {
+  createCanvas(1200,800);
+ fixedrect=createSprite(200, 200, 50, 80);
+ fixedrect.shapeColor="green"
+ fixedrect.debug=true;
+ movingrect=createSprite(400,200,80,30);
+ movingrect.shapeColor="green"
+ movingrect.debug=true
+}
+
+function draw() {
+  background(0,0,0);  
+movingrect.x=World.mouseX
+movingrect.y=World.mouseY
+if(movingrect.x-fixedrect.x<fixedrect.width/2+movingrect.width/2
+&&fixedrect.x-movingrect.x<fixedrect.width/2+movingrect.width/2
+&&fixedrect.y-movingrect.y<fixedrect.height/2+movingrect.height/2
+&&movingrect.y-fixedrect.y<fixedrect.height/2+movingrect.height/2){
+  fixedrect.shapeColor="blue"
+  movingrect.shapeColor="blue"
+}
+else{
+  fixedrect.shapeColor="green"
+  movingrect.shapeColor="green"
+}
+  drawSprites();
+}
